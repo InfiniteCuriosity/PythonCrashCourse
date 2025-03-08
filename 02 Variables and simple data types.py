d22e201e7c6637bd3e45424373e19d58ba0d3623
Russ Conte <russconte@mac.com>
2025-03-08 13:15:25 -0600

    Completion through using variables in strings

--- 02 Variables and simple data types.py
+++ 02 Variables and simple data types.py
@@ -7,6 +7,22 @@
 # print(message) # runs successfully
 
 # An example of a mistake
-message = "Hello Python Crash Course world"
-# print(mesage) wrong
-print(message) #runs successfully
+# message = "Hello Python Crash Course world"
+# # print(mesage) wrong
+# print(message) #runs successfully
+
+# 2.1 Strings
+
+# Change case in a String with Methods
+
+# name = "ada lovelace"
+# print(name.title()) # title case
+# print(name.lower())
+# print(name.upper())
+
+# 2.2 Variables in strings using f-strings
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
