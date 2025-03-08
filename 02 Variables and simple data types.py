6871575aa159d989a01766652375919a88ebef02
Russ Conte <russconte@mac.com>
2025-03-08 13:03:22 -0600

    first four code chunks run successfully

---
+++ 02 Variables and simple data types.py
@@ -0,0 +1,12 @@
+# Introduce variables in Python
+
+# message = "Hello Python world!"
+# print(message) # runs successfully
+#
+# message = "Hello Python Crash Course world!"
+# print(message) # runs successfully
+
+# An example of a mistake
+message = "Hello Python Crash Course world"
+# print(mesage) wrong
+print(message) #runs successfully
